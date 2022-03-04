ab4a7b6148dab7056fb9af10230cc0d8361e884c
huntingmarco <laquihonjannel@yahoo.com>
2022-03-04 16:16:04 -0500

    creating model

---
+++ Models/Model.cs
@@ -0,0 +1,12 @@
+namespace vega.Models
+{
+    public class Model
+    {
+        public int Id {get; set;}
+        public int Name {get; set;}
+
+        public Make Make {get; set;}
+
+        public int MakeId {get; set;}
+    }
+}
